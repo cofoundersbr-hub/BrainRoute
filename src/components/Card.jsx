ef296affb7459ea7a2698669424d27ee1c69b968
Pritamhaldar2006 <pritam2006haldar@gmail.com>
2026-04-05 16:03:22 +0530

    update 3

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,12 +4,14 @@ import './Card.css';
 export default function Card({ title, subtitle, image }) {
   return (
     <div className="card">
-      <div className="card-image">
-        <img src={image} alt={title} />
-      </div>
-      <div className="card-content">
-        <h3 className="card-title">{title}</h3>
-        <p className="card-subtitle">{subtitle}</p>
+      <div className="card-container">
+        <div className="card-content">
+          <h3 className="card-title">{title}</h3>
+          <p className="card-subtitle">{subtitle}</p>
+        </div>
+        <div className="card-image">
+          <img src={image} alt={title} />
+        </div>
       </div>
     </div>
   );
